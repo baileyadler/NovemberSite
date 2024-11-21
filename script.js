c81a7c3b4eea2d5a1eef13eb35f3d956c40be1d3
baileyadler <526BAdler@frhsd.com>
2024-11-21 09:23:41 -0500

    need to add rows /cols

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let userWelcome = alert("Welcome! You are invited!!");
+let userWelcome = alert("Welcome! You are invited!");
 let userName = prompt("name of your best friend");
 let userQuest = prompt("special occasion");
 let userDate = prompt("date for tomorrow?");
@@ -7,20 +7,20 @@ let userPlace = prompt("restaurant");
 let userTravel = prompt("mode of transportation");
 let userGift = prompt("birthday gift");
 let userDessert = prompt("birthday dessert");
-let userHouse = prompt("what is your address");
+let userHouse = prompt("write your address");
 let userTime = prompt("pick a time");
 console.log(userName);
 
-let myStory = ` <p> Hello! You are invited to <b>${userName}'s </b> Birthday party on <b>${userDate}.</b> Feel free to bring <b>${userFood} </b> to the <b>${userQuest}. </b>
-Have fun celebrating <b>${userName}</b> at ${userPlace}.</p>
+let myStory = ` <p> Hello! You are invited to <b>${userName}'s </b> Birthday party on <b>${userDate}.</b> Feel free to bring <b>${userFood} </b> to the <b>${userQuest}. </b> </p>
 
-<p> Make sure to arrive early to <b>${userPlace}. </b> At 6pm, you will leave <b>${userPlace}, </b> and travel on a <b>${userTravel}</b> to get dessert.
- After getting icecream, <b>${userName} </b> will open up her gifts and hopes to receive <b>${userGift}.</b>
+<p> Have fun celebrating <b>${userName}</b> at ${userPlace}. Make sure to arrive early to <b>${userPlace}. </b> At 6pm, we will leave <b>${userPlace}, </b> and travel on 
+a <b>${userTravel}</b> to get dessert. </p>
+ 
+<p> After getting icecream, <b>${userName} </b> will open up her gifts and hopes to receive <b>${userGift}.</b>
 When we get back to <b>${userName}'s </b> house, we will sing "HAPPY BIRTHDAY!!" and eat <b>${userDessert}</b> together. </p>
 
-<p> So please join us on <b>${userDate} </b>to celebrate <b>${userName}!</b> Please meet us at <b>${userHouse}</b> at <b>${userTime}</b> to take pictures before dinner.
-
-</p> `;
+<p> So please join us on <b>${userDate} </b> to celebrate <b>${userName}!</b> Please meet us at <b>${userHouse}</b> at <b>${userTime}</b> to take pictures before dinner.
+Please RSVP to <b>${userName}</b>55@gmail.com. </p> `;
 
 console.log(myStory);
 
